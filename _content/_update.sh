55de3f22598101976442bf29bed0b9bb71d20db6
Carlos Tacón <carlosjtacon@gmail.com>
2026-07-04 17:11:42 +1200

    ingest script

--- _content/_update.sh
+++ _content/_update.sh
@@ -12,6 +12,10 @@ category="$1"
 posts_dir="../_posts/portfolio/$category"
 images_dir="../assets/images/portfolio/$category"
 
+last_post_number=$(ls $images_dir | grep 'jpg\|mp4' | sort -V | tail -n 1)
+last_post_number=`echo "${last_post_number%.*}"`
+last_post_number=`expr "$last_post_number"`
+
 for f in *
 do
     if [ "$f" = _update.sh ]
@@ -19,20 +23,45 @@ do
         continue
     fi
 
-    input_filename="$f"
-    output_filename=$(date -r "$f" +"%Y%m%d%H%M%S")
+    last_post_number=$(( $last_post_number + 1))
 
-    date=$(date -r "$f" +"%Y-%m-%d")
-    datetime=$(date -r "$f" +"%Y-%m-%d %H:%M:%S")
+    input_filename="$f"
+    output_filename=$(printf %04d $last_post_number)
+
+    date=$(date +"%Y-%m-%d")
+    filedate=$(date -r "$f" +"%Y-%m-%d %H:%M:%S")
 
     post_filename="$posts_dir/$date-$output_filename.md"
 
+
+    if [[ "$f" == *.mp4 || "$f" == *.MP4 || "$f" == *.gif || "$f" == *.GIF ]]; then
+        output_filename_thumb="$output_filename-thumb.webm"
+        output_filename="$output_filename.mp4"
+        # Encode mp4
+        ffmpeg -i "$input_filename" -c:v libx264 "$images_dir/$output_filename"
+         # Create thumbnail
+        thumbpath="$images_dir/thumbs/$output_filename_thumb"
+        ffmpeg -i "$images_dir/$output_filename" -vf scale="${THUMB_MAX}:-1" "$thumbpath"
+    else
+        output_filename_thumb="$output_filename-thumb.webp"
+        output_filename="$output_filename.jpg"
+        # Create full quality jpg
+        magick "$input_filename" "$images_dir/$output_filename"
+
+        # Create thumbnail
+        thumbpath="$images_dir/thumbs/$output_filename_thumb"
+        magick "$f" -thumbnail "${THUMB_MAX}>" "${thumbpath}"
+    fi
+
     template="---
-date: $datetime
-filename: $output_filename.jpg
-thumbnail: $output_filename-thumb.webp
+md5sum: "$(md5sum "$input_filename")"
+filedate: "$filedate"
+filename: "$output_filename"
+thumbnail: "$output_filename_thumb"
 category: $category
 tags: $tags
+title:
+order: 0
 ---
 
 "
@@ -40,12 +69,4 @@ tags: $tags
 
     # Create post from template
     echo "$template" > $post_filename
-
-    # Create full quality jpg
-    magick "$input_filename" "$images_dir/$output_filename.jpg"
-
-    # Create thumbnail
-    thumbpath="$images_dir/thumbs/$output_filename-thumb.webp"
-    magick "$f" -thumbnail "${THUMB_MAX}>" "${thumbpath}"
-
 done
