1d99c2edccb1ff607461dbb1d24bdad75d3f54ab
Carlos Tacón <carlosjtacon@gmail.com>
2026-07-08 21:11:22 +1200

    new ingest

--- _content/_update.sh
+++ _content/_update.sh
@@ -9,64 +9,71 @@ THUMB_MAX=1280
 category="$1"
     tags="$2"
 
-posts_dir="../_posts/portfolio/$category"
-images_dir="../assets/images/portfolio/$category"
+date=$(date +"%Y-%m-%d")
+posts_dir="../../_posts/portfolio/$category"
+images_dir="../../assets/images/portfolio/$category"
 
-last_post_number=$(ls $images_dir | grep 'jpg\|mp4' | sort -V | tail -n 1)
-last_post_number=`echo "${last_post_number%.*}"`
-last_post_number=`expr "$last_post_number"`
 
-for f in *
-do
-    if [ "$f" = _update.sh ]
+for d in * ; do
+    if [ "$d" = _update.sh ]
     then
         continue
     fi
 
-    last_post_number=$(( $last_post_number + 1))
+    (
+        cd "$d"
+        arr=(${d//"_"/ })
+        number="${arr[0]}"
+        title="${arr[1]}"
+        post_filename="$posts_dir/$date-$number.md"
+        curr_images_dir="$images_dir/$number"
+        echo Creating directory "$curr_images_dir"
+        mkdir "$curr_images_dir"
+        counter=0
+        for f in * ; do
+            if [ "$f" = thumb.* ] ; then
+                if [[ "$f" == *.mp4 || "$f" == *.MP4 || "$f" == *.gif || "$f" == *.GIF ]]; then
+                    thumbpath="$curr_images_dir/thumb.webm"
+                    echo Creating thumbnail "$f" "$thumbpath"
+                    ffmpeg -i "$f" -vf scale="${THUMB_MAX}:-1" "$thumbpath"
+                else
+                    thumbpath="$curr_images_dir/thumb.webp"
+                    echo Creating thumbnail "$f" "$thumbpath"
+                    magick "$f" -thumbnail "${THUMB_MAX}>" "${thumbpath}"
+                fi
+                continue
+            fi
+            counter=$((counter+1))
+            input_filename="$f"
+            output_filename=$(printf %04d $counter)
 
-    input_filename="$f"
-    output_filename=$(printf %04d $last_post_number)
+            if [[ "$f" == *.mp4 || "$f" == *.MP4 || "$f" == *.gif || "$f" == *.GIF ]]; then
+                output_filename="$output_filename.mp4"
+                echo Creating file "$curr_images_dir/$output_filename"
+                ffmpeg -i "$f" -c:v libx264 "$curr_images_dir/$output_filename"
+            else
+                output_filename="$output_filename.jpg"
+                echo Creating file "$curr_images_dir/$output_filename"
+                magick "$f" "$curr_images_dir/$output_filename"
+            fi
+        done
 
-    date=$(date +"%Y-%m-%d")
-    filedate=$(date -r "$f" +"%Y-%m-%d %H:%M:%S")
-
-    post_filename="$posts_dir/$date-$output_filename.md"
-
-
-    if [[ "$f" == *.mp4 || "$f" == *.MP4 || "$f" == *.gif || "$f" == *.GIF ]]; then
-        output_filename_thumb="$output_filename-thumb.webm"
-        output_filename="$output_filename.mp4"
-        # Encode mp4
-        ffmpeg -i "$input_filename" -c:v libx264 "$images_dir/$output_filename"
-         # Create thumbnail
-        thumbpath="$images_dir/thumbs/$output_filename_thumb"
-        ffmpeg -i "$images_dir/$output_filename" -vf scale="${THUMB_MAX}:-1" "$thumbpath"
-    else
-        output_filename_thumb="$output_filename-thumb.webp"
-        output_filename="$output_filename.jpg"
-        # Create full quality jpg
-        magick "$input_filename" "$images_dir/$output_filename"
-
-        # Create thumbnail
-        thumbpath="$images_dir/thumbs/$output_filename_thumb"
-        magick "$f" -thumbnail "${THUMB_MAX}>" "${thumbpath}"
-    fi
-
-    template="---
-number: "$output_filename"
-title:
+        template="---
+number: '"$number"'
+title: "$title"
 category: $category
 tags: $tags
 featured: 0
 ---
 
 "
-    echo "$input_filename..."
+        echo Creating post "$post_filename"
 
-    # Create post from template
-    echo "$template" > $post_filename
+        # Create post from template
+        echo "$template" > $post_filename
 
-    # Delete ingested files
-    rm "$f"
+        # Delete ingested files
+        cd ..
+        rm -r "$d"
+    );
 done
